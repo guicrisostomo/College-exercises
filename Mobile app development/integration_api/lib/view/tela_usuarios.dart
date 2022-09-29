3aa4fe92b74fceb58761c97971df572fa1157372
Guilherme Crisostomo <guilherme.silva9872@gmail.com>
2022-09-29 08:19:38 -0300

    feat: add structure basic

---
+++ Mobile app development/integration_api/lib/view/tela_usuarios.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../model/usuario.dart';
+
+class TelaUsuario extends StatefulWidget {
+  const TelaUsuario({super.key});
+
+  @override
+  State<TelaUsuario> createState() => _TelaUsuarioState();
+}
+
+class _TelaUsuarioState extends State<TelaUsuario> {
+  List<Usuario> dados = [];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
