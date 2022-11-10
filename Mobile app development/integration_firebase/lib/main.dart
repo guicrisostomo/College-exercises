0acaa24706852c63f8f32f2dce4401e452ac3ad9
Guilherme Crisostomo <guilherme.silva9872@gmail.com>
2022-11-10 08:24:41 -0300

    feat: add new pages

--- Mobile app development/integration_firebase/lib/main.dart
+++ Mobile app development/integration_firebase/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:integration_firebase/pages/login_page.dart';
+import 'package:integration_firebase/pages/criar_conta_page.dart';
+import 'package:integration_firebase/pages/principal_page.dart';
 
 import 'firebase_options.dart';
 
@@ -22,6 +24,8 @@ Future<void> main() async {
       initialRoute: 'login',
       routes: {
         'login' : (((context) => const LoginPage())),
+        'criar_conta': ((context) => const CriarContaPage()),
+        'principal': ((context) => const TelaPrincipal()),
       },
     ),
   );
---
+++ Mobile app development/integration_firebase/lib/pages/principal_page.dart
@@ -0,0 +1,138 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_typing_uninitialized_variables
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../controller/login_controller.dart';
+import 'tarefas_andamento_page.dart';
+import 'tarefas_concluidas_page.dart';
+import 'tarefas_page.dart';
+
+class TelaPrincipal extends StatefulWidget {
+  const TelaPrincipal({Key? key}) : super(key: key);
+
+  @override
+  State<TelaPrincipal> createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  var tarefas;
+
+  // Telas de Navegecao
+  final List<Widget> telas = [
+    TarefasPage(),
+    TarefasAndamentoPage(),
+    TarefasConcluidasPage(),
+  ];
+  int index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 70,
+        title: Row(
+          children: [
+            Expanded(
+              child: Text(
+                'Tarefas',
+                style: GoogleFonts.kanit(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(
+                  Icons.account_box,
+                  size: 14,
+                ),
+
+                //
+                // TODO: Retornar usuário logado
+                //
+                FutureBuilder<String>(
+                  future: LoginController().retornarUsuarioLogado(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return CircularProgressIndicator();
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.done) {
+                      if (snapshot.hasError) {
+                        return const Text('Error');
+                      } else if (snapshot.hasData) {
+                        return Text(
+                          snapshot.data.toString(),
+                          overflow: TextOverflow.ellipsis,
+                          style: GoogleFonts.roboto(
+                            fontSize: 16,
+                          ),
+                        );
+                      } else {
+                        return const Text('Empty data');
+                      }
+                    } else {
+                      return Text('State: ${snapshot.connectionState}');
+                    }
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+        backgroundColor: Colors.blueAccent.shade700,
+        actions: [
+          IconButton(
+            tooltip: 'sair',
+            onPressed: () {
+              LoginController().logout();
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                'login',
+                (Route<dynamic> route) => false,
+              );
+            },
+            icon: Icon(Icons.logout),
+          ),
+        ],
+      ),
+      body: telas[index],
+      bottomNavigationBar: BottomNavigationBar(
+        iconSize: 40,
+        currentIndex: index,
+        onTap: (idx) {
+          setState(() {
+            index = idx;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.task_outlined,
+            ),
+            label: 'Tarefas',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.electric_bolt_outlined,
+            ),
+            label: 'Em andamento',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.check_circle_outline,
+            ),
+            label: 'Concluído',
+          ),
+        ],
+      ),
+    );
+  }
+}
